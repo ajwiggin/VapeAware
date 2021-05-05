a390967a153c31115f50e8d5502851d5f507c14a
Andrew Wiggin <andrew.j.wiggin@gmail.com>
2021-05-05 15:29:37 -0800

    Add jsdoc documentation for firebase methods

--- src/server/db.js
+++ src/server/db.js
@@ -5,19 +5,33 @@ const dev = process.env.NODE_ENV === "development";
 
 export const db = Firebase.initializeApp(config);
 
-export const writeData = async (ref='/', data) {
+/**
+ * Writes an object to firebase. If null is passed in, the data at the specified location will be removed
+ * @param {String} ref Data reference location
+ * @param {?Object} data JSON data to write
+ */
+export const writeData = async (ref='/', data) => {
     Firebase.database().ref(ref).set(data)
         .then(() => { if (dev) console.log(`Data written to ref '${ref}'`) })
         .catch(() => { if (dev) console.error(`Data write failed to ref '${ref}'`) });
 }
 
+/**
+ * Retrieves firebase data at specified location
+ * @param {String} ref Data reference location
+ * @returns Data JSON
+ */
 export const readData = async (ref='/') => {
     return Firebase.database().ref(ref)
         .on('value', snapshot => snapshot.val());
 }
 
+/**
+ * Get's the participant's current location
+ * @returns Position JSON or null (error)
+ */
 export const getLocation = async () => {
-    navigator.geolocation.getCurrentPosition(
+    return navigator.geolocation.getCurrentPosition(
         position => position, // should full location be logged or only coords"? position.coords
         error => console.error(error));
 }
